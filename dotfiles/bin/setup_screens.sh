d1029b04c49d9dc9dcf6660f57567d0c8fdec58e
samjy <sam+git@samjy.com>
2019-10-17 15:41:47 +0200

    Improve utility scripts

--- dotfiles/bin/setup_screens.sh
+++ dotfiles/bin/setup_screens.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#external=HDMI1
+#internal=eDP1
+external=HDMI-0
+internal=eDP-1-1
+
 if [ $# -ne 2 ]
 then
 	if [ $# -eq 1 ]
@@ -8,8 +13,8 @@ then
 			"off")
 				echo "Turning extra screen off"
 				# turn screen off
-				xrandr --output HDMI1 --off
-				xrandr --output eDP1 --pos 0x0
+				xrandr --output $external --off
+				xrandr --output $internal --pos 0x0
 				exit 0
 				;;
 		esac
@@ -25,21 +30,21 @@ resolution=$2
 case $1 in
 	"right")
 		echo "setting to right"
-		xrandr --output HDMI1 --mode $resolution --pos 1920x0
+		xrandr --output $external --mode $resolution --pos 1920x0
 		;;
 	"left")
 		echo "setting to left"
-		xrandr --output HDMI1 --mode $resolution --rotate normal --pos 0x0
-		xrandr --output eDP1 --pos ${resolution%x*}x0
+		xrandr --output $external --mode $resolution --rotate normal --pos 0x0
+		xrandr --output $internal --pos ${resolution%x*}x0
 		;;
 	"leftvert")
 		echo "setting to left vertical"
-		xrandr --output HDMI1 --rotate right --mode $resolution --pos 0x0
-		xrandr --output eDP1 --pos ${resolution##*x}x0
+		xrandr --output $external --rotate right --mode $resolution --pos 0x0
+		xrandr --output $internal --pos ${resolution##*x}x0
 		;;
 	"top")
 		echo "setting to top"
-		xrandr --output HDMI1 --mode $resolution --pos 0x0
-		xrandr --output eDP1 --pos 0x${resolution##*x}
+		xrandr --output $external --mode $resolution --pos 0x0
+		xrandr --output $internal --pos 0x${resolution##*x}
 		;;
 esac
