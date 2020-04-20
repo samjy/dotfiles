51561ed4c1315b8a2f8eabc9a0bc0796b385b70a
samjy <sam+git@samjy.com>
2020-04-20 21:34:54 +0200

    Improve setup_screens script

--- dotfiles/bin/setup_screens.sh
+++ dotfiles/bin/setup_screens.sh
@@ -5,7 +5,7 @@
 external=HDMI-0
 internal=eDP-1-1
 
-if [ $# -ne 2 ]
+if [ $# -lt 2 ]  # we need at least 2 args
 then
 	if [ $# -eq 1 ]
 	then
@@ -15,16 +15,19 @@ then
 				# turn screen off
 				xrandr --output $external --off
 				xrandr --output $internal --pos 0x0
+				# make the internal output the primary
+				xrandr -- output $internal --primary
 				exit 0
 				;;
 		esac
 	fi
 
-	echo "$0 right|left|top|off [resolution]"
+	echo "$0 right|left|top|off <resolution>"
 	xrandr
 	exit
 fi
 
+# TODO autodetect and use the higher res for screen?
 resolution=$2
 
 case $1 in
@@ -48,3 +51,19 @@ case $1 in
 		xrandr --output $internal --pos 0x${resolution##*x}
 		;;
 esac
+
+case $3 in
+	"")
+		echo "with external screen as primary"
+		echo "(for presentations, add 'presentation' to the command)"
+		xrandr --output $external --primary
+		;;
+	"presentation")
+		echo "with internal screen as primary (good for presentations)"
+		xrandr --output $internal --primary
+		;;
+esac
+
+# TODO use better xrandr commands to avoid errors
+#xrandr --output VGA1 --mode 1024x768 --same-as LVDS1
+#xrandr --output VGA1 --mode 1024x768 --right-of LVDS1
